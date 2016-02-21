589bc41bc6a0c3b8a2cd11a56b4cf9a061d6a761
Anuja Verma <anuja.verma@gmail.com>
2016-02-21 01:38:34 -0800

    dropdown toggle

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -1,18 +1,33 @@
 // We need to show a button and a list.
 // This component should know when to show the list based on when the user clicks on a button.
 
+
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function(){
-    alert('hello from dropdown');
+    this.setState({open: !this.state.open})
   },
 
+  getInitialState: function(){
+    return {open: false}
+  },
   render: function(){
+    var list = this.props.items.map(function(item){
+      return <ListItem item={item} />
+    });
+
     return <div className="dropdown">
-      <Button whenClicked={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret" />
+      <Button whenClicked={this.handleClick}
+      className="btn-default"
+      title={this.props.title}
+      subTitleClassName="caret"
+      />
+      <ul className={"dropdown-menu " +  (this.state.open? "show" : "")}>
+        {list}
+      </ul>
     </div>
   }
 });
