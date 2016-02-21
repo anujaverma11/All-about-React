27113893bf8ca542069fa108b7e6cab6e525998c
Anuja Verma <anuja.verma@gmail.com>
2016-02-20 23:05:08 -0800

    Add solution to permuatation

--- dropdown/src/button.jsx
+++ dropdown/src/button.jsx
@@ -2,12 +2,8 @@ var React = require('react');
 
 
 module.exports = React.createClass({
-  handleClick: function(){
-    alert('button clicked');
-  },
-
   render: function(){
-    return <button onClick={this.handleClick} className={"btn " + this.props.className} type="button">
+    return <button onClick={this.props.whenClicked} className={"btn " + this.props.className} type="button">
       {this.props.title}
       <span className={this.props.subTitleClassName}>{this.props.subTitle}</span>
     </button>
