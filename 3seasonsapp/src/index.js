cf62c15526e62877351e6797a650a760ed36c70b
anujaverma11 <anuja.verma@gmail.com>
2020-06-04 23:57:46 -0700

    Class based componenets

--- 3seasonsapp/src/index.js
+++ 3seasonsapp/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
+
+
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
     position => console.log(position),
     err => console.log(err)
     );
-
-  return (<div> Hi!! </div>)
+    return <div>Latitude: </div>;
+  }
 }
 
-
 ReactDom.render(<App />, document.querySelector('#root'));
+
+{/*
+1. JS File loaded by browser
+2. App component gets created
+3. We call geolocation service
+4. App returns JSX, gets rendered to page as HTML
+5. We get results of geolocation
+
+Rules of Class Components
+ - Must be A Javascript Class
+ - Must extend (subclass) React.Component
+ - Must define a 'render' method that returns some amount of JSX
+*/}
